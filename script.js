f38d6886eafe9b22898630750f1ff8fbb81077a0
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-08-30 22:43:42 +0530

    Simple server to get all and get by id

---
+++ script.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const Joi = require("joi"); //used for validation
+const app = express();
+app.use(express.json());
+
+const questions = [
+  {
+    id: 1,
+    question: "What is the capital of India?",
+    options: ["Delhi", "Mumbai", "Chennai", "Kolkata"],
+    answer: "Delhi",
+  },
+  {
+    id: 2,
+    question: "What is the capital of USA?",
+    options: ["New York", "Washington", "Chicago", "Los Angeles"],
+    answer: "Washington",
+  },
+  {
+    id: 3,
+    question: "What is the capital of Canada?",
+    options: ["Ottawa", "Toronto", "Vancouver", "Montreal"],
+    answer: "Ottawa",
+  },
+  {
+    id: 4,
+    question: "What is the capital of Australia?",
+    options: ["Sydney", "Melbourne", "Brisbane", "Perth"],
+    answer: "Sydney",
+  },
+];
+
+//READ Request Handlers
+app.get("/", (req, res) => {
+  res.send("Quizziz Server!!");
+});
+
+app.get("/api/questions", (req, res) => {
+  res.send(questions);
+});
+
+app.get("/api/questions/:id", (req, res) => {
+  const question = questions.find(
+    (element) => element.id === parseInt(req.params.id)
+  );
+
+  if (!question)
+    res
+      .status(404)
+      .send(
+        '<h2 style="font-family: Popins; color: #000;">Ooops... Cant find what you are looking for!</h2>'
+      );
+  res.send(question);
+});
+
+//PORT ENVIRONMENT VARIABLE
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
